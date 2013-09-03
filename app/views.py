d6a93b89e40a2cfe8c31ca831dce1a6f28c28400
nyghtowl <warrick.melanie@gmail.com>
2013-09-03 01:09:47 -0500

    Reworking templates to fit into framework. Adding templates to views. Pulled out some content under layout that will not be used at this time in addition to about us.

--- app/views.py
+++ app/views.py
@@ -18,3 +18,42 @@ def internal_error(error):
 def index():
     return render_template('index.html')
 
+@app.route('/index_2')
+def index_2():
+    return render_template('index_2.html')
+
+@app.route('/index_3')
+def index_3():
+    return render_template('index_3.html')
+
+@app.route('/index_4')
+def index_4():
+    return render_template('index_4.html')
+
+@app.route('/portfolio_2')
+def portfolio_2():
+    return render_template('portfolio_2.html')
+
+@app.route('/portfolio_3')
+def portfolio_3():
+    return render_template('portfolio_3.html')
+
+@app.route('/portfolio_4')
+def portfolio_4():
+    return render_template('portfolio_4.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/single_project')
+def single_project():
+    return render_template('single_project.html')
